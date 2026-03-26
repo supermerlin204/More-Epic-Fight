af5cbe9ad3c4c9d1e946a19f0cee325033e60d5e
Arcadia <1919463065@qq.com>
2026-03-27 02:39:49 +0800

    拆迁思索(假入留下来了)

--- src/main/java/org/merlin204/mef/event/ClientModEvents.java
+++ src/main/java/org/merlin204/mef/event/ClientModEvents.java
@@ -1,11 +1,9 @@
 package org.merlin204.mef.event;
 
-import net.createmod.ponder.foundation.PonderIndex;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import org.merlin204.mef.client.ponder.MEFPonderPlugin;
 import org.merlin204.mef.main.MoreEpicFightMod;
 
 @Mod.EventBusSubscriber(modid = MoreEpicFightMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -13,8 +11,6 @@ public class ClientModEvents {
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
-       event.enqueueWork(() -> {
-           PonderIndex.addPlugin(new MEFPonderPlugin());
-       });
+       event.enqueueWork(() -> {});
     }
 }
--- src/main/java/org/merlin204/mef/mixin/epicfight/AnimationPlayerMixin.java
+++ src/main/java/org/merlin204/mef/mixin/epicfight/AnimationPlayerMixin.java
@@ -1,12 +1,8 @@
 package org.merlin204.mef.mixin.epicfight;
 
-import net.createmod.ponder.api.level.PonderLevel;
-import net.minecraft.nbt.CompoundTag;
 import org.merlin204.mef.api.entity.MEFEntityAPI;
-import org.merlin204.mef.api.ponder.EpicFightSceneBuilder;
 import org.merlin204.mef.capability.MEFCapabilities;
 import org.merlin204.mef.capability.MEFEntity;
-import org.merlin204.mef.main.MoreEpicFightMod;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,15 +19,6 @@ public class AnimationPlayerMixin {
             ordinal = 0
     )
     private float mef$applyAnimationSpeed(float playbackSpeed, LivingEntityPatch<?> entityPatch) {
-        if(MoreEpicFightMod.isPonderLoaded() && entityPatch.isLogicalClient()) {
-            if(entityPatch.getOriginal().level() instanceof PonderLevel) {
-                CompoundTag data = entityPatch.getOriginal().getPersistentData();
-                if(data.contains(EpicFightSceneBuilder.PLAY_SPEED)) {
-                    return data.getFloat(EpicFightSceneBuilder.PLAY_SPEED);
-                }
-            }
-        }
-
         if (MEFEntityAPI.getStaminaTypeByEntity(entityPatch.getOriginal()) != null) {
             MEFEntity mefEntity = MEFCapabilities.getMEFEntity(entityPatch.getOriginal());
             float speedMultiplier = mefEntity.getAnimationSpeed();
